f8e94733a9b5bf9c773ea9c1e8b4e283f92ff0f1
moldo <mr.vrabie@gmail.com>
2024-07-21 14:45:14 +0200

    adding a unit test

--- src/main/java/xyz/optimized/recursivefun/algos/medium/QuickSortSorting.java
+++ src/main/java/xyz/optimized/recursivefun/algos/medium/QuickSortSorting.java
@@ -8,34 +8,36 @@ public class QuickSortSorting {
     public static void main(String[] args) {
 int[] arr = {10, 7, 8, 9, 1, 5};
         int n = arr.length;
-        quickSort(arr, 0, n - 1);
+        quickSort(arr);
         printArray(arr);
     }
 
-    private static void printArray(int[] arr) {
+    public static void printArray(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
 
-    private static void quickSort(int[] arr, int i, int i1) {
-        if (i >= i1) return;
-        int pivot = arr[i1];
-        int low = i - 1;
-        int high = i;
-        while (high < i1) {
+    public static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int i1, int i2) {
+        if (i1 >= i2) return;
+        int pivot = arr[i2];
+        int low = i1 - 1;
+        int high = i1;
+        while (high < i2) {
             if (arr[high] >= pivot) {
                 high++;
             } else {
-                low++;
-                swap(arr, low, high);
-                high++;
+                swap(arr, ++low, high++);
             }
         }
-        swap(arr, low + 1, i1);
+        swap(arr, low + 1, i2);
         quickSort(arr, 0, low);
-        quickSort(arr, low + 2, i1);
+        quickSort(arr, low + 2, i2);
     }
 
     private static void swap(int[] arr, int low, int high) {
